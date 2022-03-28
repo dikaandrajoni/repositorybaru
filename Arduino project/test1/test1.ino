895e54dce322caea9ab7c759620d31246cd6fb60
arincantikam26 <arincantikam@gmail.com>
2022-03-28 22:42:28 +0700

    new update arin

--- Arduino project/test1/test1.ino
+++ Arduino project/test1/test1.ino
@@ -1,11 +1,20 @@
 int a=3;
+int b=4;
+
 void setup() {
 pinMode(a,OUTPUT);
+pinMode(b, OUTPUT);
+
 }
 
 void loop() {
   digitalWrite(a,1);
   delay(250);
   digitalWrite(a,0);
+  delay(250);
+
+  digitalWrite(b,1);
+  delay(250);
+  digitalWrite(b,0);
   delay(250);
 }
